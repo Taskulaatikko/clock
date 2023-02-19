00d9893873cd2ad77cc7635bb8826532cb30a309
Taskulaatikko <n2hasa@students.oamk.fi>
2023-02-19 14:35:46 +0200

    Shows the current clock time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [time, setTime] = useState(new Date())
+
+  /*function updateTime() {
+    setTime (new Date())
+  }*/
+
+useEffect(() => {
+  //const id = setInterval(updateTime, 1000)
+  const id = setInterval(() => {
+    setTime(new Date())
+  })
+  return() => {
+    clearInterval(id)
+  }
+},[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div style={{margin: '30px'}}>
+  <p>Kello on: {time.toLocaleTimeString()}</p>
+</div>
   );
 }
 
